edff81dae187fc225a93b06095ec81c65eda3833
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2025-10-19 19:17:25 -0300

    Subindo Estrutura do BurguerReact

---
+++ src/components/Layouts/Layout.js
@@ -0,0 +1,16 @@
+import React, { Children } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Layout({Children}){
+    return(
+        <>
+         <Header />
+         <div>{Children}</div>
+         <Footer />
+        </>
+    )
+}
+
+
+export default Layout;
