3cd0a19c15c3a7ef5ccac11bcc31aa6be6a53f07
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2025-11-27 10:22:47 -0300

    criando pagina Dev

--- src/pages/Contato/Section1.js
+++ src/pages/Contato/Section1.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import Burguer from "../../Assets/assets/hero/hero-2.png";
+import Dev from "../../Assets/assets/Dev/dev.png";
 import { Link } from "react-router-dom";
 
 
@@ -10,19 +10,13 @@ function Section1(){
             <Row>
                 <Col lg={7} className="mb-5 mb-lg-0">
                     <div className="position-relative">
-                        <img src={Burguer} className="img-fluid" alt="Hero" />
-                        <div className="price_badge">
-                            <div className="badge_text">
-                                <h4 className="h4_xs">Apenas</h4>
-                                <h3 className="h3_lg">R$26,99</h3>
-                            </div>
-                        </div>
+                        <img src={Dev} className="img-fluid" alt="Hero" />
                     </div>
                 </Col>
                 <Col lg={5}>
                     <div className="hero_text text-center">
-                        <h1 className="text-white">Novo Burguer</h1>
-                        <h2 className="text-white">Aneis acebolados</h2>
+                        <h1 className="text-white">Desenvolvedor Front-end</h1>
+                        <h2 className="text-white">José Neto</h2>
                         <p className="text-white pt-2">Hambúrguer Artesanal de Cebola, preparado com carne selecionada, pão brioche macio e cebolas caramelizadas no ponto perfeito.
                          Uma combinação suculenta, saborosa e irresistível para quem busca um verdadeiro gourmet. </p>
                          <Link to="/" className="btn order_now">
